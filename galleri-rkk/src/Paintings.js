a3646a139a17af5d6d1e1cd77acbfd6d14b94f50
kanestrom <108542057+kanestrom@users.noreply.github.com>
2023-07-27 16:22:52 +0200

    Update Paintings.js

--- galleri-rkk/src/Paintings.js
+++ galleri-rkk/src/Paintings.js
@@ -6,7 +6,8 @@ function Paintings() {
     <div style={{ maxWidth: "800px", margin: "auto", textAlign: "center", marginTop: 32, marginBottom: 64 }}>
 {/* <h1 style={{ marginTop: 64, marginBottom: 32 }}>Malerier</h1> */}
       <img src={headerimg} style={{ display: "flex", width: "100%" }} alt="headerimg" />
-
+      <img src={IMG_6463} style={{ display: "flex", width: "100%" }} alt="IMG_6463" />
+      <img src={FullSizeRender} style={{ display: "flex", width: "100%" }} alt="FullSizeRender" />
     </div>
   );
 }
