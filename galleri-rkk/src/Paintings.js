4829a7ec3aaceb2b499165002e66b404da1e427d
kanestrom <108542057+kanestrom@users.noreply.github.com>
2023-07-27 16:04:05 +0200

    Update Paintings.js

--- galleri-rkk/src/Paintings.js
+++ galleri-rkk/src/Paintings.js
@@ -1,10 +1,10 @@
 import headerimg from './clean.jpg';
 import React from 'react';
-
+// Kode for å kommentere vekk: To slasher for hel linje utenfor en funksjon (javascript). Men inni en funksjon, dvs inni "return" er det HTML, og der er tegnene: {/* i starten og */} på slutten.
 function Paintings() {
   return (
     <div style={{ maxWidth: "800px", margin: "auto", textAlign: "center", marginTop: 32, marginBottom: 64 }}>
-      <h1 style={{ marginTop: 64, marginBottom: 32 }}>Malerier</h1>
+{/* <h1 style={{ marginTop: 64, marginBottom: 32 }}>Malerier</h1> */}
       <img src={headerimg} style={{ display: "flex", width: "100%" }} alt="headerimg" />
 
     </div>
